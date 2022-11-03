601d018e21a53bdad36af5ecfd55449c4a478265
Aryan Tayal <bookworms@ariansh.com>
2022-11-03 13:50:26 +0530

    get random developer excuses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, {useState} from "react"
+import excuses from "excuses"
 import Ball from "./Ball"
 
 const App = () => {
+  const randomExcuse = () => excuses.developers.getRandom();
+  const handleBallClick = () => setExcuse(randomExcuse());
+
+  const [excuse, setExcuse] = useState(randomExcuse());
   return (
 		<div className="App">
-      <Ball excuse="Some random excuse goes here"/>
+      <Ball excuse={excuse} handleClick={handleBallClick}/>
     </div>
   )
 }
