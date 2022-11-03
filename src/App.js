04690bd820238e1513431fc728384f32313b0629
Aryan Tayal <bookworms@ariansh.com>
2022-11-03 14:03:52 +0530

    add basic styles to app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react"
-import excuses from "excuses"
-import Ball from "./Ball"
+import React, { useState } from "react";
+import excuses from "excuses";
+import Ball from "./Ball";
+import "./App.css";
 
 const App = () => {
   const randomExcuse = () => excuses.developers.getRandom();
@@ -8,10 +9,16 @@ const App = () => {
 
   const [excuse, setExcuse] = useState(randomExcuse());
   return (
-		<div className="App">
-      <Ball excuse={excuse} handleClick={handleBallClick}/>
+    <div className="App">
+      <h2 className="App-heading">Magical Ball</h2>
+      <Ball excuse={excuse} handleClick={handleBallClick} />
+      <h2 className="App-heading">Of Excuses</h2>
+      <p className="App-description">
+        Never do your developer work again and get out of anything with an iron
+        clad excuse. Click the ball to get your excuse.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App  
+export default App;
